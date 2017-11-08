7d7e6bf959e3c570f2846cac94c3bef6d334fc94
toybreaker <rokmabox@gmail.com>
2017-11-08 21:33:10 +0800

    first deploy

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" class="container" >
+
+    <SourceSelection v-on:sourceChanged="sourceChanged"></SourceSelection>
+    <Newslist v-bind:source="source"></Newslist>
+    <SourceYears v-on:sourceChanged="sourceChanged"></SourceYears>
+
+  </div>
+</template>
+
+<script>
+import Newslist from './components/Newslist'
+import SourceSelection from './components/SourceSelection'
+export default {
+  name: 'app',
+  components: {
+    Newslist,
+    SourceSelection
+  },
+  data () {
+    return {
+      source: ""
+    }
+  },
+  methods: {
+    sourceChanged: function (source) {
+      this.source = source;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+ padding-top: 20px;
+}
+</style>
